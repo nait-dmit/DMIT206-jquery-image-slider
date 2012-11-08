bacce7d6eb9b7504cfd2c776ce674d946908168f
Christian Naths <christiannaths@gmail.com>
2012-11-08 00:11:56 -0700

    added condition to test for end position

--- assets/javascripts/jquery.slider.js
+++ assets/javascripts/jquery.slider.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
 
   var slider = $('.slider');
+  var start_position = 0;
+  var end_position = -1000;
 
   setInterval(function(){
-    slider.animate({ 'left': "-=500px" });
+
+    if (slider.position().left <= end_position) {
+      slider.animate({ 'left': start_position });
+    } else {
+      slider.animate({ 'left': "-=500px" });
+    };
+
   }, 3000);
 
 });
