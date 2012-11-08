40f6e2fd81ad6ffe647d80dca23bdbfa7f05b8ea
Christian Naths <christiannaths@gmail.com>
2012-11-07 23:49:35 -0700

    changed to relative adjustments

--- assets/javascripts/jquery.slider.js
+++ assets/javascripts/jquery.slider.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var slider = $('.slider');
 
   setInterval(function(){
-    slider.animate({ 'left': -500 });
+    slider.animate({ 'left': "-=500px" });
   }, 3000);
 
 });
